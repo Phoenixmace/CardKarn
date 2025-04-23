66f672e77c245484640b1c116e2547298eebae57
maxce <phoenixmace3@gmail.com>
2025-04-23 15:49:28 +0200

    push for save

--- util/database_util.py
+++ util/database_util.py
@@ -1,4 +1,4 @@
-import ijson
+import json
 from util import data_util
 from util import json_util
 from collections import defaultdict
@@ -7,27 +7,34 @@ def default_dict_tree():
     return defaultdict(default_dict_tree)
 
 
-def get_parsed_objects(break_index = None):
-    return_list = []
-    with open(data_util.get_data_path('card_database.json'), 'r', encoding='utf-8') as f:
-        parser = ijson.items(f, 'item')
-        for index, object in enumerate(parser):
-            return_list.append([index,object])
-            if break_index and break_index < index:
-                return return_list
-    return return_list
 
 def update_index_map():
     index_map = default_dict_tree()
-    card_list = get_parsed_objects()
-    for object in card_list:
-        card_index = object[0]
-        card_dict = object[1]
-        price = card_dict['prices']['eur']
-        if isinstance(price, str):
-            price = float(price)
-        collector_number = card_dict['collector_number']
-        index_map[card_dict['name'].lower()][card_dict['set'].lower()][collector_number.lower()] = {'index':card_index,'price':price}
-        index_map[card_dict['id']] = card_index
+    file = open(data_util.get_data_path('source_card_database.json'), encoding='utf-8')
+    lines = file.readlines()
 
+    # iterate over lines
+    for line_number, line in enumerate(lines):
+        if len(line)>5 and '{' in line and '}' in line:
+            line_string = str(line[line.find('{'):line.rfind('}')+1])
+            line_dict = json.loads(line_string)
+            price = line_dict['prices']['eur']
+            name_key = line_dict['name'].lower()
+            set_key = line_dict['set'].lower()
+            if isinstance(price, str):
+                price = float(price)
+            collector_number = line_dict['collector_number']
+            index_map[name_key][set_key][collector_number.lower()] = {'index':line_number+1,'price':price}
+            # log cheapest versions
+            cheapest_overall = index_map[name_key].get('cheapest_index')
+            if isinstance(price, float) and (not cheapest_overall or cheapest_overall['price'] > price):
+                index_map[name_key]['cheapest_index'] = {'index': line_number + 1, 'price': price}
+
+            # Second level (cheapest in set)
+            cheapest_in_set = index_map[name_key][set_key].get('cheapest_index')
+            if isinstance(price, float) and (not cheapest_in_set or cheapest_in_set['price'] > price):
+                index_map[name_key][set_key]['cheapest_index'] = {'index': line_number + 1, 'price': price}
+            # save id
+            index_map[line_dict['id']] = line_number+1
     json_util.dump_data('database_index_map.json', index_map)
+update_index_map()
--- util/process_time_tracker.py
+++ util/process_time_tracker.py
@@ -10,6 +10,24 @@ class Time_tracker:
         print_string = f'{name} time is: {round(time.time()-self.start, 2)}'
         print(print_string)
 
+class Loop_time:
+    def __init__(self, name='', print_frequency=10):
+        self.name = name
+        self.print_frequency = print_frequency
+        self.loop_number = 1
+        self.loop_times = []
+    def loop_start(self):
+        self.loop_start_time = time.time()
+    def loop_end(self):
+        self.loop_times.append(time.time()-self.loop_start_time)
+        self.loop_times = self.loop_times[:-30]
+        self.loop_number += 1
+        if self.loop_number%self.print_frequency == 0:
+            print_string = f'average {self.name} time is: {round(sum(self.loop_times)/len(self.loop_times), 2)}'
+            print(print_string)
+
+
+
 
 
 
