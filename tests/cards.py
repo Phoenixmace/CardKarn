d401e2180ac4392f661c33736699112b8a1ee56b
maxce <phoenixmace3@gmail.com>
2025-06-03 13:38:23 +0200

    finished np array

--- tests/cards.py
+++ tests/cards.py
@@ -1,6 +1,6 @@
 from util.database import sql_card_operations
 from models.Card import BaseCard
-card = BaseCard({'name': 'Etali, Primal Conqueror'}, wait_for_salt_score=True)
+card = BaseCard({'name': 'Etali, Primal Conqueror // Etali, Primal Sickness'}, wait_for_salt_score=True)
 print(card.get_np_array())
 
 
--- util/machine_learning/card_array.py
+++ util/machine_learning/card_array.py
@@ -38,8 +38,49 @@ def get_combined_attribute(card_item, attribute):
     elif hasattr(card_item, 'card_faces'):
         values = []
         for i in card_item.card_faces:
-            if attribute in card_item.card_faces[i]:
-                attribute_1_value =
+            if attribute in i:
+                values.append(i[attribute])
+        if len(values) == 1:
+            return values[0]
+        elif len(values) == 2:
+            # combine
+            if attribute == 'cmc':
+                return sum(values) / len(values)
+
+            elif attribute == 'mana_cost':
+                return values[0]
+            elif attribute == 'oracle_text':
+                return ' // '.join(values)
+            # typeline
+            elif attribute == 'type_line':
+                split_typeline_1 = values[0].split(' — ')
+                split_typeline_2 = values[1].split(' — ')
+                types = split_typeline_1[0] + ' ' + split_typeline_2[0]
+                subtypes = []
+                if len(split_typeline_1) == 2:
+                    subtypes.append(split_typeline_1[1])
+                if len(split_typeline_2) == 2:
+                    subtypes.append(split_typeline_2[1])
+                subtypes = ' '.join(subtypes)
+                return types + ' — ' + subtypes
+
+            elif attribute == 'power' or attribute == 'toughness':
+                values = [int(i) for i in values]
+                return sum(values) / len(values)
+            elif isinstance(values[0], str):
+                return ' // '.join(values)
+            elif isinstance(values[0], int) or isinstance(values[0], float):
+                return sum(values) / len(values)
+            else:
+                return values[0]
+
+
+        else:
+            return None
+
+    else:
+        return None
+
 
 
 def np_array_1(card_item, **weights):
@@ -47,12 +88,13 @@ def np_array_1(card_item, **weights):
 
         # cmc
 
-        cmc = card_item.cmc
+        cmc = get_combined_attribute(card_item, 'cmc')
         array.append(cmc)
 
         # pips
 
-        colored_pips = card_item.mana_cost.count('{')
+        pips = get_combined_attribute(card_item, 'mana_cost')
+        colored_pips = pips.count('{')
         array.append(colored_pips)
 
         # color_idendity
@@ -63,7 +105,7 @@ def np_array_1(card_item, **weights):
 
         # typeline
 
-        typeline = card_item.type_line
+        typeline = get_combined_attribute(card_item, 'type_line')
 
         # supertypes
 
@@ -99,13 +141,14 @@ def np_array_1(card_item, **weights):
                 array.append(-1)
 
         # power/toughness
-
-        if hasattr(card_item, 'power'):
-            array.append(card_item.power)
+        power = get_combined_attribute(card_item, 'power')
+        if isinstance(power, int):
+            array.append(power)
         else:
             array.append(-1)
-        if hasattr(card_item, 'toughness'):
-            array.append(card_item.toughness)
+        toughness = get_combined_attribute(card_item, 'toughness')
+        if isinstance(toughness, int):
+            array.append(toughness)
         else:
             array.append(-1)
         # edhrec_rank
@@ -141,7 +184,7 @@ def np_array_1(card_item, **weights):
         # rarity
 
         rarity = card_item.rarity
-        rarity_category = categorise(rarity, 'rarity', True)
+        rarity_category = categorise(rarity, 'rarity', False)
         array.append(rarity_category)
 
         # gamechanger
@@ -153,10 +196,16 @@ def np_array_1(card_item, **weights):
 
         # oracle text
 
-        oracle_text_list = card_item.oracle_text.split(' ')
+        oracle_text = get_combined_attribute(card_item, 'oracle_text')
+        oracle_text_list = oracle_text.split(' ')
+        cleaned_oracle_text_list = []
+        for string in oracle_text_list:
+            for substring in string.split('\n'):
+                cleaned_oracle_text_list.append(substring)
+
         for i in range(60):
-            if i < len(oracle_text_list):
-                oracle_text = oracle_text_list[i]
+            if i < len(cleaned_oracle_text_list):
+                oracle_text = cleaned_oracle_text_list[i]
                 oracle_text_int = categorise(oracle_text, 'oracle_text', False)
                 array.append(oracle_text_int)
             else:
