8d0d4c55e4944828718b13cc442d22efb09e8c69
Phoenixmace <128027330+Phoenixmace@users.noreply.github.com>
2025-06-24 17:05:58 +0200

    user var for collection

--- website/backend/user_pages.py
+++ website/backend/user_pages.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify, make_response, redirect, url_for, render_template, session
 from util.database.sql_util import get_cursor
-
-
+import os
+import config
 user_bp = Blueprint('user_bp', __name__)
+UPLOAD_FOLDER = os.path.join(config.data_folder_path, 'website', 'user_uploads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @user_bp.route('/collection/<username>')
 def user_collection(username):
@@ -14,3 +16,19 @@ def user_profile(username):
     if not user or user['name'] != username:
         return redirect(url_for('login_bp.login'))
     return render_template('profile.html', user=user)
+
+
+
+@user_bp.route('/upload', methods=['POST'])
+def upload_file():
+    if 'csvFile' not in request.files:
+        return 'Keine Datei gefunden', 400
+
+    file = request.files['csvFile']
+    if file.filename == '':
+        return 'Leere Datei', 400
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+    return f'Datei {file.filename} erfolgreich gespeichert!', 200
+
